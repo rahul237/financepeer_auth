b4fe14dfa08fb20fb246c9a8ab920dbeaeee30e1
rahul237 <rahulbhushanmehta@gmail.com>
2021-12-03 06:14:13 +0530

    parsed json to upload

--- fpclient/src/screens/Upload.js
+++ fpclient/src/screens/Upload.js
@@ -7,10 +7,17 @@ export default function Upload({ children }) {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = e => {
-      console.log("e.target.result", e.target.result);
+      // console.log("e.target.result", e.target.result);
+      const list = (JSON.parse(e.target.result))
+
+      list.map(elem => {
+        console.log(elem["id"])
+        
+      });
       setFiles(e.target.result);
     };
   };
+
   return (
     <>
       <h1>Upload Json file - Example</h1>
